a01b0a5ce9804e3691f5bf931a5412db4405aac8
Braian <braiannickolas12@gmail.com>
2024-03-05 10:29:52 -0300

    Aula232
    Mais conceitos de Orientacao a objeto

--- js/index.js
+++ js/index.js
@@ -1,31 +1,11 @@
-const routes = {
-  "/": "./pages/home.html",
-  "/about": "./pages/about.html",
-  "/contact": "/pages/contact.html",
-  404: "/pages/404.html",
+import './router.js'
 
-
-}
-
-function route(event) {
-  event = event || window.event
-  event.preventDefault()
-
-  window.history.pushState({}, "", event.target.href)
-
-  handle()
-}
-
-function handle() {
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route)
-    .then(data => data.text())
-    .then(html => {
-      document.querySelector('#app').innerHTML = html
-    })
-}
+//const routes = {
+//"/": "./pages/home.html",
+//"/about": "./pages/about.html",
+// "/contact": "/pages/contact.html",
+// 404: "/pages/404.html",
+//}
 
 handle()
 
